f34ba6e5843fbc918864be37dc27ee91c056544a
Giorgos Giourmetakis <giorgosggg@gmail.com>
2019-06-28 13:16:23 +0300

    changes

--- src/app/components/shared/filter.pipe.ts
+++ src/app/components/shared/filter.pipe.ts
@@ -3,9 +3,10 @@ import { Pipe, PipeTransform } from '@angular/core'
 @Pipe({name: 'movieFilter'})
 export class MovieFilterPipe implements PipeTransform {
   transform(items: any[], criteria: string): any {
-    if(criteria === 'all'){ return items } else
-    return items.filter(item =>{
-      return item.category === criteria;
-    });
+    if(criteria === 'all'){ return items } 
+    else 
+      return items.filter(item => {
+        return item.category === criteria;
+      });
   }
 }
