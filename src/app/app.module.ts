0192ffce2e58097009c44396beebd072b14d67dd
Giorgos Giourmetakis <giorgosggg@gmail.com>
2019-06-28 13:41:06 +0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { MoviesService } from './services/movies.service';
-import { MovieFilterPipe } from './components/shared/filter.pipe';
+import { MovieFilterPipe } from './pipes/filter.pipe';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './components/movies.component';
 import { MovieDetailsComponent } from './components/movie-details.component';
