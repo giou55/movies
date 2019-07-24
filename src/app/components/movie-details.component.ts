f3f9ced7495be7abffb9e8b03369187bd914ee96
Giorgos Giourmetakis <giorgosggg@gmail.com>
2019-07-24 13:27:29 +0300

    changes

--- src/app/components/movie-details.component.ts
+++ src/app/components/movie-details.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MoviesService } from '../services/movies.service';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   templateUrl: './movie-details.component.html',
@@ -9,9 +10,13 @@ import { ActivatedRoute } from '@angular/router';
 export class MovieDetailsComponent {
   movie:any;
 
-  constructor(private moviesService: MoviesService, private route: ActivatedRoute) { }
+  constructor(private moviesService: MoviesService, private route: ActivatedRoute, private _location: Location) { }
 
   ngOnInit(){
     this.movie = this.moviesService.getMovie(this.route.snapshot.params['id'])
+  }
+
+  backClicked() {
+    this._location.back();
   }
 }
